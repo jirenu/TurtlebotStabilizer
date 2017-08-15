eb519461871cb29d33ba9587998769236def30bf
jishnu97 <jishnu@vt.edu>
2017-08-15 12:21:12 -0400

    SARSA

--- src/beginner_tutorials/src/listener2.cpp
+++ src/beginner_tutorials/src/listener2.cpp
@@ -48,32 +48,41 @@ int act5 = 0;
 int act6 = 0;
 
 StateReward stateReward;
-//ros::NodeHandle np;
 ros::Publisher pub;
 ros::Publisher pubRLEnv;
 ros::Publisher pubRLState;
+
+float cancelNoise(float val, float low, float high)
+{
+        if(val>= low && val<=high)
+        {
+                return 0;
+        }
+        return val;
+}
+
 
 float rewardCalculator(StateReward st)
 {
 	numReward++;
 	float maxReward = 10;
-        float beta = 0.5;
-        float lossImuSquared = pow(10*st.imu1LinearX - 10*st.imu2LinearX, 2) +
-                               pow(10*st.imu1LinearZ - 10*st.imu2LinearZ, 2);
+        float beta = 0.4;
+        float lossImuSquared = pow(100*st.imu1LinearX - 100*st.imu2LinearX, 2) +
+                               pow(100*st.imu1LinearZ - 100*st.imu2LinearZ, 2);
         float lossImu = pow(lossImuSquared, 0.5);
-        float lossTeleopSquared = pow(10*st.teleopLinearXIn - 10*st.teleopLinearXOut, 2) +
-                               pow(10*st.teleopAngularZIn - 10*st.teleopAngularZOut, 2);
+        float lossTeleopSquared = pow(100*st.teleopLinearXIn - 100*st.teleopLinearXOut, 2) +
+                               pow(100*st.teleopAngularZIn - 100*st.teleopAngularZOut, 2);
         float lossTeleop  = pow(lossTeleopSquared, 0.5);
         float loss = beta*lossImu+(1-beta)*lossTeleop;
 	float reward = maxReward -loss;
 	if (reward<0.0)
 	{
 		totalReward +=0.0;
-        	printf( "Running average is: %6.4lf\n", totalReward/numReward);
+        	//printf( "Running average is: %6.4lf\n", totalReward/numReward);
 		return 0.0;
 	}
 	totalReward +=reward;
-	printf( "Running average is: %6.4lf\n", totalReward/numReward);
+	//printf( "Running average is: %6.4lf\n", totalReward/numReward);
 	return reward;
 }
 
@@ -116,12 +125,13 @@ void cmd_vel_Callback_teleop(const geometry_msgs::Twist& cmd_vel)
  // pub.publish(new_cmd_vel);
   //Prep the message to be sent
   rl_msgs::RLStateReward stateVar;
+  
   stateVar.state.push_back(10*stateReward.odomLinearX);
   stateVar.state.push_back(10*stateReward.odomAngularZ);
   stateVar.state.push_back(10*stateReward.teleopLinearXIn);
   stateVar.state.push_back(10*stateReward.teleopAngularZIn);
   stateVar.reward = rewardCalculator(stateReward);
-  //publish the message
+//publish the message
   pubRLState.publish(stateVar);
 
 }
@@ -130,15 +140,15 @@ void cmd_vel_Callback_teleop(const geometry_msgs::Twist& cmd_vel)
 void cmd_vel_Callback_odom(const nav_msgs::Odometry& odom)
 {
   stateReward.odomLinearX = odom.twist.twist.linear.x;
-  stateReward.odomAngularZ = odom.twist.twist.angular.z;
+  stateReward.odomAngularZ = odom.twist.twist.angular.z+0.002;
   //ROS_INFO("I heard odom: [%f]", odom.twist.twist.linear.x);
 }
 
 void imu1_Callback(const sensor_msgs::Imu& imu1)
 {
-//  stateReward.imu1LinearX = imu1.linear_acceleration.x;
+  stateReward.imu1LinearX = cancelNoise(imu1.linear_acceleration.x, 0.0, -0.22);
   //stateReward.imu1LinearY = imu1.linear_acceleration.y;
-  stateReward.imu1LinearZ = imu1.linear_acceleration.z;
+  stateReward.imu1LinearZ = cancelNoise(imu1.linear_acceleration.z,0.0, -0.23);
 
  // ROS_INFO("I heard imu1 x acc: [%f]", imu1.linear_acceleration.x);
  // ROS_INFO("I heard imu1 y acc: [%f]", imu1.linear_acceleration.y);
@@ -148,9 +158,9 @@ void imu1_Callback(const sensor_msgs::Imu& imu1)
 
 void imu2_Callback(const sensor_msgs::Imu& imu2)
 {
-  //stateReward.imu2LinearX = imu2.linear_acceleration.x;
+  stateReward.imu2LinearX = cancelNoise(imu2.linear_acceleration.x,0.0,-0.1);
   //stateReward.imu2LinearY = imu2.linear_acceleration.y;
-  stateReward.imu2LinearZ = imu2.linear_acceleration.z;
+  stateReward.imu2LinearZ = cancelNoise(imu2.linear_acceleration.z,0.0,-0.09);
  
  //ROS_INFO("I heard imu2 x acc: [%f]", imu2.linear_acceleration.x);
   //ROS_INFO("I heard imu2 y acc: [%f]", imu2.linear_acceleration.y);
