6b252d06f5348bc98b873ca00d9d651c67c518ad
jishnu97 <jishnu@vt.edu>
2017-08-25 13:44:12 -0400

    https://github.com/jishnu97/TurlebotStabilizer.git

--- src/beginner_tutorials/src/listener2.cpp
+++ src/beginner_tutorials/src/listener2.cpp
@@ -29,7 +29,7 @@ float odomLinearX;
 float odomAngularZ;
 
 };
-
+std::clock_t start;
 int actionFrequency[6];
 float prev_linear_x;
 float prev_linear_y;
@@ -40,6 +40,8 @@ float prev_angular_z;
 float totalReward = 0.0;
 float numReward = 0.0;
 float alpha = .5;
+float oldLinAcc = 0.0;
+float oldAngAcc = 0.0;
 int act1 = 0;
 int act2 = 0;
 int act3 = 0;
@@ -52,6 +54,24 @@ ros::Publisher pub;
 ros::Publisher pubRLEnv;
 ros::Publisher pubRLState;
 
+void startTimer()
+{
+	start = std::clock();
+}
+double stopTimer()
+{
+	double duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	start = std::clock();
+	return duration;
+}
+float calculateDerivative(float acc1, float acc2, double deltaTime)
+{
+	if(deltaTime == 0)
+	{
+		return 0.0;
+	}	
+	return (acc2-acc1)/(float)deltaTime;
+}
 float cancelNoise(float val, float low, float high)
 {
         if(val>= low && val<=high)
@@ -60,30 +80,29 @@ float cancelNoise(float val, float low, float high)
         }
         return val;
 }
-
-
 float rewardCalculator(StateReward st)
 {
-	numReward++;
-	float maxReward = 10;
-        float beta = 0.4;
-        float lossImuSquared = pow(100*st.imu1LinearX - 100*st.imu2LinearX, 2) +
-                               pow(100*st.imu1LinearZ - 100*st.imu2LinearZ, 2);
+        numReward++;
+        float maxReward = 10;
+        float beta = 0.5;
+        float lossImuSquared = pow(st.imu2LinearX - st.imu1LinearX, 2) +
+                               pow(st.imu2LinearZ - st.imu1LinearZ, 2);
         float lossImu = pow(lossImuSquared, 0.5);
-        float lossTeleopSquared = pow(100*st.teleopLinearXIn - 100*st.teleopLinearXOut, 2) +
-                               pow(100*st.teleopAngularZIn - 100*st.teleopAngularZOut, 2);
+        float lossTeleopSquared = pow(10*st.teleopLinearXIn - 10*st.teleopLinearXOut, 2) +
+                               pow(10*st.teleopAngularZIn - 10*st.teleopAngularZOut, 2);
         float lossTeleop  = pow(lossTeleopSquared, 0.5);
         float loss = beta*lossImu+(1-beta)*lossTeleop;
-	float reward = maxReward -loss;
+        float reward = maxReward -loss;
+	printf("IMU:%f Teleop:%f\n", lossImu, lossTeleop);
 	if (reward<0.0)
-	{
-		totalReward +=0.0;
-        	//printf( "Running average is: %6.4lf\n", totalReward/numReward);
-		return 0.0;
-	}
-	totalReward +=reward;
-	//printf( "Running average is: %6.4lf\n", totalReward/numReward);
-	return reward;
+        {
+                totalReward +=0.0;
+                //printf( "Running average is: %6.4lf\n", totalReward/numReward);
+                return 0.0;
+        }
+        totalReward +=reward;
+        //printf( "Running average is: %6.4lf\n", totalReward/numReward);
+        return reward;
 }
 
 
@@ -92,46 +111,27 @@ float rewardCalculator(StateReward st)
  */
 void cmd_vel_Callback_teleop(const geometry_msgs::Twist& cmd_vel)
 {
-  //ROS_INFO("I heard: [%f]", cmd_vel.linear.x);
-/*
-  geometry_msgs::Twist new_cmd_vel;
-
-  new_cmd_vel.linear.x = (alpha * cmd_vel.linear.x) + (1-alpha)*prev_linear_x;
-  new_cmd_vel.linear.y = (alpha * cmd_vel.linear.y) + (1-alpha)*prev_linear_y;
-  new_cmd_vel.linear.z = (alpha * cmd_vel.linear.z) + (1-alpha)*prev_linear_z;
-
-  new_cmd_vel.angular.x = (alpha * cmd_vel.angular.x) + (1-alpha)*prev_angular_x;
-  new_cmd_vel.angular.y = (alpha * cmd_vel.angular.y) + (1-alpha)*prev_angular_y;
-  new_cmd_vel.angular.z = (alpha * cmd_vel.angular.z) + (1-alpha)*prev_angular_z;
-
-  prev_linear_x = new_cmd_vel.linear.x;
-  prev_linear_y = new_cmd_vel.linear.y;
-  prev_linear_z = new_cmd_vel.linear.z;
-  prev_angular_x = new_cmd_vel.angular.x;
-  prev_angular_y = new_cmd_vel.angular.y;
-  prev_angular_z = new_cmd_vel.angular.z;
-*/
-  //ROS_INFO("I sent: [%f]", new_cmd_vel.linear.x);
- 
-
   //Set struct values
   stateReward.teleopLinearXIn = cmd_vel.linear.x;
   stateReward.teleopAngularZIn = cmd_vel.angular.z; 
-  
-//TODO: publish to RL_State_Reward
-//  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  float dTime = stopTimer(); 
+  float jerkLin = calculateDerivative(oldLinAcc, stateReward.odomLinearX, dTime);
+  float jerkAng = calculateDerivative(oldAngAcc, stateReward.odomAngularZ,dTime);
+  oldLinAcc = stateReward.odomLinearX;
+  oldAngAcc = stateReward.odomAngularZ;
+  startTimer();
 
-//  ros::Publisher pub = n.advertise<geometry_msgs::Twist>("mobile_base_nodelet_manager",1);
- // pub.publish(new_cmd_vel);
-  //Prep the message to be sent
+  printf("Lin:%f ang:%f\n", jerkLin, jerkAng);
   rl_msgs::RLStateReward stateVar;
-  
+  //Set values to the state vector
   stateVar.state.push_back(10*stateReward.odomLinearX);
   stateVar.state.push_back(10*stateReward.odomAngularZ);
   stateVar.state.push_back(10*stateReward.teleopLinearXIn);
   stateVar.state.push_back(10*stateReward.teleopAngularZIn);
+  stateVar.state.push_back(10*jerkLin);
+  stateVar.state.push_back(10*jerkAng);
   stateVar.reward = rewardCalculator(stateReward);
-//publish the message
+  //publish the message
   pubRLState.publish(stateVar);
 
 }
@@ -139,36 +139,27 @@ void cmd_vel_Callback_teleop(const geometry_msgs::Twist& cmd_vel)
 
 void cmd_vel_Callback_odom(const nav_msgs::Odometry& odom)
 {
+  //Set linear and angular accelerations in the struct
   stateReward.odomLinearX = odom.twist.twist.linear.x;
   stateReward.odomAngularZ = odom.twist.twist.angular.z+0.002;
-  //ROS_INFO("I heard odom: [%f]", odom.twist.twist.linear.x);
 }
 
 void imu1_Callback(const sensor_msgs::Imu& imu1)
 {
-  stateReward.imu1LinearX = cancelNoise(imu1.linear_acceleration.x, 0.0, -0.22);
-  //stateReward.imu1LinearY = imu1.linear_acceleration.y;
-  stateReward.imu1LinearZ = cancelNoise(imu1.linear_acceleration.z,0.0, -0.23);
-
- // ROS_INFO("I heard imu1 x acc: [%f]", imu1.linear_acceleration.x);
- // ROS_INFO("I heard imu1 y acc: [%f]", imu1.linear_acceleration.y);
-  //ROS_INFO("I heard imu1 z acc: [%f]", imu1.linear_acceleration.z);
+  stateReward.imu1LinearX = imu1.linear_acceleration.x;
+  stateReward.imu1LinearZ = imu1.linear_acceleration.z;
    
 }
 
 void imu2_Callback(const sensor_msgs::Imu& imu2)
 {
   stateReward.imu2LinearX = cancelNoise(imu2.linear_acceleration.x,0.0,-0.1);
-  //stateReward.imu2LinearY = imu2.linear_acceleration.y;
   stateReward.imu2LinearZ = cancelNoise(imu2.linear_acceleration.z,0.0,-0.09);
- 
- //ROS_INFO("I heard imu2 x acc: [%f]", imu2.linear_acceleration.x);
-  //ROS_INFO("I heard imu2 y acc: [%f]", imu2.linear_acceleration.y);
-  //ROS_INFO("I heard imu3 z acc: [%f]", imu2.linear_acceleration.z);
-
 }
 void actPrinter()
 {
+	//Prints the number and type of actions taken by the agent 
+	printf("New\n");
 	printf("Action 1(low lin):%d\n", act1);
 	printf("Action 2(med lin):%d\n", act2);
 	printf("Action 3(high lin):%d\n", act3);
@@ -184,7 +175,9 @@ void RLAction_Callback(const rl_msgs::RLAction& act)
 	float maxOdom = 0.18;
 	float minOdom = -0.18;
 	geometry_msgs::Twist new_cmd_vel;
-	if (act.action==0)
+	//Sends the appropriate action to the turtlebot depending on the
+	// command sent by the agent
+	if (act.action==0) 
 	{
 		act1++;
 		new_cmd_vel.linear.x = low*maxOdom;
@@ -229,14 +222,13 @@ void RLAction_Callback(const rl_msgs::RLAction& act)
 	stateReward.teleopLinearXOut = new_cmd_vel.linear.x;	
 	stateReward.teleopAngularZOut = new_cmd_vel.angular.z;
 	pub.publish(new_cmd_vel);
+	//Print out the actions
 	actPrinter();
-	//ROS_INFO("I sent: [%f]", new_cmd_vel.linear.x);
-	//ROS_INFO("I heard from agent: [%f]", act.action);
 }
 
 void initStruct(StateReward* st)
 {
-        //Teleop values
+//Initialize the struct
 st->teleopLinearXIn = 0;
 st->teleopAngularZIn = 0;
 st->teleopLinearXOut = 0;
@@ -257,47 +249,14 @@ st->odomAngularZ = 0;
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+  startTimer();
   ros::init(argc, argv, "listener2");
-initStruct(&stateReward);
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-//  ros::NodeHandle n;
-
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
+  initStruct(&stateReward);
   ros::NodeHandle n;
-  ros::Subscriber sub_teleop = n.subscribe("new_topic", 1000, cmd_vel_Callback_teleop);
   ros::Subscriber sub_odom = n.subscribe("odom", 1000, cmd_vel_Callback_odom);
   ros::Subscriber sub_imu1 = n.subscribe("/sim1/imu/data_raw", 1000, imu1_Callback);
   ros::Subscriber sub_imu2 = n.subscribe("/sim2/imu/data_raw", 1000, imu2_Callback);
   
-  //Subscribing from reinforcement learning agent
   ros::Subscriber sub_RLAction = n.subscribe("/rl_agent/rl_action",1000, RLAction_Callback);
   
   pub = n.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop",1); 
@@ -307,32 +266,13 @@ initStruct(&stateReward);
   pubRLState = n.advertise<rl_msgs::RLStateReward>("/rl_env/rl_state_reward", 1);
   //Advertises to rl_msgs/RLEnvDescription Message
   pubRLEnv = n.advertise<rl_msgs::RLEnvDescription>("/rl_env/rl_env_description", 1, true);
-   //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("", 1000);  
 
-//Advertises to rl_msgs/RLStateReward.msg
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-//  ros::Publisher pub = n.advertise<geometry_msgs::Twist>("mobile_base_nodelet_manager",1);
-//  pub.publish(cmd_vel);
-
-//TODO: Publish to env
 	rl_msgs::RLEnvDescription envDesc;
+	//Sets up the environment for the agent
 	envDesc.num_actions = 6;
-	envDesc.num_states = 16;
-	envDesc.min_state_range.push_back(float(-0.18));
-	envDesc.min_state_range.push_back(float(-0.9));
-	envDesc.min_state_range.push_back(float(-0.2));
-        envDesc.min_state_range.push_back(float(-1.0));
-	
-	envDesc.max_state_range.push_back(float(0.18));
-        envDesc.max_state_range.push_back(float(0.9));
-	envDesc.max_state_range.push_back(float(0.2));	
-	envDesc.max_state_range.push_back(float(1.0));
-	envDesc.max_reward = 100;
-	envDesc.reward_range = 100;
+	envDesc.num_states = 2000;
+	envDesc.max_reward = 10;
+	envDesc.reward_range = 10;
 	envDesc.stochastic = false;
 	envDesc.episodic = false;
 	pubRLEnv.publish(envDesc);
